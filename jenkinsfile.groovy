af0242dd372855467f84ef2f89de784d014bf5d3
Michael Lehmann <lehmamic@bluewin.ch>
2020-02-12 22:23:24 +0100

    Pimp the pod template

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,11 +1,14 @@
 podTemplate(label: "dotnet-31", 
                     cloud: "openshift", 
-                    inheritFrom: "maven", 
+//                    inheritFrom: "maven", 
                     containers: [
             containerTemplate(name: "jnlp", 
                               image: "registry.redhat.io/dotnet/dotnet-31-jenkins-agent-rhel7:latest", 
                               resourceRequestMemory: "512Mi", 
-                              resourceLimitMemory: "512Mi", 
+                              resourceLimitMemory: "512Mi",
+                              resourceRequestCpu: "500m",
+                              resourceLimitCpu: "2",
+                              ttyEnabled: true,
                               envVars: [
               envVar(key: "CONTAINER_HEAP_PERCENT", value: "0.25") 
             ])
