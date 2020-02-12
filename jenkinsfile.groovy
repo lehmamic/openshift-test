4e86388ec6fd695f6bded0bd86db31d19cdfa50b
Michael Lehmann <lehmamic@bluewin.ch>
2020-02-12 21:29:49 +0100

    Use a config map for the new dotnet jenkins slave

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,29 +1,10 @@
-String label = "worker-${UUID.randomUUID().toString()}"
 
-podTemplate(label: label,
-        cloud: "openshift",
-        containers: [
-                containerTemplate(
-                        name: "dotnetcore",
-                        image: "image-registry.openshift-image-registry.svc:5000/devopsfusionleh/dotnet-jenkins-slave",
-                        alwaysPullImage: true,
-                        resourceRequestMemory: "2Gi",
-                        resourceRequestCpu: "500m",
-                        resourceLimitMemory: "2Gi",
-                        resourceLimitCpu: "1",
-                        command: "cat",
-                        ttyEnabled: true,
-                        envVars: []),
-        ],
-        volumes: []
-) {
-    node(label) {
-        stage("build") {
-            echo 'dotnet build'
-        }
+node("dotnet-31") {
+    stage("build") {
+        echo 'dotnet build'
+    }
 
-        stage("test") {
-            echo 'dotnet test'
-        }
+    stage("test") {
+        echo 'dotnet test'
     }
 }
