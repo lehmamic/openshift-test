158deea83603a0a69371d6f0759ebd25cdfccee2
Michael Lehmann <lehmamic@bluewin.ch>
2020-02-12 22:16:57 +0100

    Try an encample with podtemplate dsl

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,9 +1,22 @@
-node("dotnet-31") {
-    stage("build") {
-        echo 'dotnet build'
-    }
+podTemplate(label: "dotnet-31", 
+                    cloud: "openshift", 
+                    inheritFrom: "maven", 
+                    containers: [
+            containerTemplate(name: "jnlp", 
+                              image: "registry.redhat.io/dotnet/dotnet-31-jenkins-agent-rhel7:latest", 
+                              resourceRequestMemory: "512Mi", 
+                              resourceLimitMemory: "512Mi", 
+                              envVars: [
+              envVar(key: "CONTAINER_HEAP_PERCENT", value: "0.25") 
+            ])
+          ]) {
+    node("dotnet-31") {
+        stage("build") {
+            echo 'dotnet build'
+        }
 
-    stage("test") {
-        echo 'dotnet test'
+        stage("test") {
+            echo 'dotnet test'
+        }
     }
 }
