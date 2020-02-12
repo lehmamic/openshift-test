7ab811c204010559f3bd4572d62afd192d454a66
Michael Lehmann <lehmamic@bluewin.ch>
2020-02-12 21:51:21 +0100

    change to node dotnet-31

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,29 +1,9 @@
-String label = "worker-${UUID.randomUUID().toString()}"
+node("dotnet-31") {
+    stage("build") {
+        echo 'dotnet build'
+    }
 
-podTemplate(label: label,
-        cloud: "openshift",
-        containers: [
-                containerTemplate(
-                        name: "dotnetcore",
-                        image: "registry.redhat.io/dotnet/dotnet-31-jenkins-agent-rhel7",
-                        alwaysPullImage: true,
-                        resourceRequestMemory: "2Gi",
-                        resourceRequestCpu: "500m",
-                        resourceLimitMemory: "2Gi",
-                        resourceLimitCpu: "1",
-                        command: "cat",
-                        ttyEnabled: true,
-                        envVars: []),
-        ],
-        volumes: []
-) {
-    node(label) {
-        stage("build") {
-            echo 'dotnet build'
-        }
-
-        stage("test") {
-            echo 'dotnet test'
-        }
+    stage("test") {
+        echo 'dotnet test'
     }
 }
