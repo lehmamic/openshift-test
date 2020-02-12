2a926a4f6982b8034c2d6e238802fb9783f60c2f
Michael Lehmann <lehmamic@bluewin.ch>
2020-02-12 23:41:40 +0100

    correct typings and document the checkout and build steps

--- README.md
+++ README.md
@@ -89,3 +89,45 @@ podTemplate(label: "dotnet-31",
     }
 }
 ```
+
+## Git clone and build the app
+
+```groovy
+podTemplate(label: "dotnet-31",
+                    cloud: "openshift",
+                    inheritFrom: "maven",
+                    containers: [
+            containerTemplate(name: "jnlp",
+                              image: "registry.redhat.io/dotnet/dotnet-31-jenkins-agent-rhel7:latest",
+                              resourceRequestMemory: "512Mi",
+                              resourceLimitMemory: "512Mi",
+                              resourceRequestCpu: "500m",
+                              resourceLimitCpu: "2",
+                              ttyEnabled: true,
+                              envVars: [
+              envVar(key: "CONTAINER_HEAP_PERCENT", value: "0.25")
+            ])
+          ]) {
+    node("dotnet-31") {
+        stage("checkout") {
+          sh 'printenv'
+          git([
+              url:"${GIT_REPO}",
+              branch:"${GIT_BRANCH}"
+          ]) 
+        }
+
+        stage("restore") {
+            sh 'dotnet restore src/Zuehlke.OpenShiftDemo.sln'
+        }
+
+        stage("build") {
+            sh 'dotnet build src/Zuehlke.OpenShiftDemo.sln -c Release --no-restore'
+        }
+
+        stage("publish") {
+            sh 'dotnet publish src/Zuehlke.OpenShiftDemo/Zuehlke.OpenShiftDemo.csproj -c Release -o ./app/publish --no-restore --no-build'
+        }
+    }
+}
+```
--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,16 +1,16 @@
-podTemplate(label: "dotnet-31", 
-                    cloud: "openshift", 
-                    inheritFrom: "maven", 
+podTemplate(label: "dotnet-31",
+                    cloud: "openshift",
+                    inheritFrom: "maven",
                     containers: [
-            containerTemplate(name: "jnlp", 
-                              image: "registry.redhat.io/dotnet/dotnet-31-jenkins-agent-rhel7:latest", 
-                              resourceRequestMemory: "512Mi", 
+            containerTemplate(name: "jnlp",
+                              image: "registry.redhat.io/dotnet/dotnet-31-jenkins-agent-rhel7:latest",
+                              resourceRequestMemory: "512Mi",
                               resourceLimitMemory: "512Mi",
                               resourceRequestCpu: "500m",
                               resourceLimitCpu: "2",
                               ttyEnabled: true,
                               envVars: [
-              envVar(key: "CONTAINER_HEAP_PERCENT", value: "0.25") 
+              envVar(key: "CONTAINER_HEAP_PERCENT", value: "0.25")
             ])
           ]) {
     node("dotnet-31") {
@@ -30,12 +30,8 @@ podTemplate(label: "dotnet-31",
             sh 'dotnet build src/Zuehlke.OpenShiftDemo.sln -c Release --no-restore'
         }
 
-        stage("publich") {
+        stage("publish") {
             sh 'dotnet publish src/Zuehlke.OpenShiftDemo/Zuehlke.OpenShiftDemo.csproj -c Release -o ./app/publish --no-restore --no-build'
-        }
-
-        stage("test") {
-            echo 'dotnet test'
         }
     }
 }
